1cee60874f250a32d3901c4c1d63041885539d49
Isaac <75605951+potential1energy@users.noreply.github.com>
2023-12-09 02:35:20 +1300

    Add files via upload

---
+++ GenZ.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#define sti std
+
+#define nowbe =
+#define mf const
+#define skibidi unsigned
+#define chonk long
+#define skull int
+#define rizz float
+#define chonkrizz double
+#define deez bool
+#define whatthedawgdoin void
+#define haur char
+#define tbomb sti::string
+#define	krrakrra sti::vector
+#define waffle sti::cout
+#define shuddup sti::endl
+#define sus if
+#define imposter else
+#define nah !=
+#define fr ==
+#define btw &&
+#define carenot |
+#define yall for
+#define smol <
+#define thicc >
+#define nocap true
+#define cap false
+#define fuckaround try
+#define findout catch
+#define hood namespace
+#define pluh throw
+#define skidadle return
+#define addy &
+#define ovathere *
